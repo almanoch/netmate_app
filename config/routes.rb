ede870be12bd23bbdf5550b37d4f6349cd062bfd
Anatoliy Khan <aktee@mail.ru>
2012-05-01 08:36:58 -0700

    Add a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 NetmateApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/help"
+
+  get "pages/about"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
