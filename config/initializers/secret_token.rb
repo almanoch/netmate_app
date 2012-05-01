5ad472859b9189e48dd4bdb6fb29b3edb04adbb7
Anatoliy Khan <aktee@mail.ru>
2012-05-01 07:50:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetmateApp::Application.config.secret_token = 'fffff5e217072bd8313d0d2849710d3c068acfb4efc19f23b4553fdbaf9bde2fab2cbcd8666134f2519d2c8ef67b00fc207fbea103be0f32c6ad6ef9d71faac6'
