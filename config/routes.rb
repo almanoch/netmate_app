3ca165cb09696795904137c03765bed4f3452389
Anatoliy Khan <aktee@mail.ru>
2012-05-16 10:21:10 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 NetmateApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
-  get "users/new"
-
   root to: 'pages#home'
   
-  match'/signup',   to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help',    to: 'pages#help'
-
   match '/about',   to: 'pages#about'
-
   match '/contact', to: 'pages#contact'
 
   # The priority is based upon order of creation:
